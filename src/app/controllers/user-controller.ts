d95fd666b6e2b61a4c6976b7bdb8226af61e1703
Rafael Fischer <rafaelsanfischer@gmail.com>
2020-11-23 17:10:28 -0300

    added get users method

--- src/app/controllers/user-controller.ts
+++ src/app/controllers/user-controller.ts
@@ -2,18 +2,23 @@ import { Request, Response } from 'express'
 import User from '../../database/models/User'
 import { formatResponse } from '../helpers'
 
-export async function createUser(req: Request, res: Response) {
+export async function createUser(req: Request, res: Response) {    
     try {
-        const body: IUserBody = req.body
+        const body: IUserBody = req.body        
         const response = await createUserService(body)
         return res.status(response.status).json(response.data)
-    } catch(err) {
+    } catch(err) {        
         return res.status(500).json(err)
     }    
 }
 
 export async function getUsers(req: Request, res: Response) {
-
+    try {
+        const response = await getUsersService()
+        return res.status(response.status).json(response.data)
+    } catch(err) {
+        return res.status(500).json(err)
+    }
 }
 
 export async function deleteUser(req: Request, res: Response) {
@@ -27,4 +32,10 @@ async function createUserService(body: IUserBody) {
     if (hasUser) return formatResponse(409, { message: 'User already exists' })
     const user = await User.create({ email })
     return formatResponse(201, user)
+}
+
+async function getUsersService() {
+    const users = await User.find()
+    if (users.length === 0) return formatResponse(404, { message: 'No users found' })
+    return formatResponse(200, users)
 }
