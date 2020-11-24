1065c378f4fb66876808c4721816aa6ff56c1d99
Rafael Fischer <rafaelsanfischer@gmail.com>
2020-11-24 09:55:06 -0300

    created stringToArray formater helper

--- src/app/helpers/index.ts
+++ src/app/helpers/index.ts
@@ -4,3 +4,12 @@ export function formatResponse(status: number, data?: object) {
         data: data
     }
 }
+
+export function covertStringToArray(techs: string | undefined) {
+    if (techs) {
+        const splitedString = techs.split(',')
+        const newArray = splitedString.map(item => item.trim())
+        return newArray
+    }
+    return []
+}
