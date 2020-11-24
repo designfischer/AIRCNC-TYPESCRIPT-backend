9b2b10a298025dd6063986ef760f3ea6654e7749
Rafael Fischer <rafaelsanfischer@gmail.com>
2020-11-24 09:35:00 -0300

    added create session method to router

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,9 +1,12 @@
 import { Router } from 'express'
+import { createSession } from '../controllers/session-controller'
 import { createUser, getUsers, deleteUser } from '../controllers/user-controller'
 
 const routes = Router()
 
 routes.get('/', (req, res) => res.send('API Aircnc in Typescript'))
+
+routes.post('/sessions', createSession)
 
 routes.get('/users', getUsers)
 routes.post('/users', createUser)
