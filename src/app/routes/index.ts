ebc190f5febcbb59bfb9991a5b9cb8030fbe6207
Rafael Fischer <rafaelsanfischer@gmail.com>
2020-11-24 09:58:28 -0300

    added createSpot route

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express'
 import { createSession } from '../controllers/session-controller'
+import { createSpot } from '../controllers/spot-controller'
 import { createUser, getUsers, deleteUser } from '../controllers/user-controller'
 
 const routes = Router()
@@ -11,5 +12,7 @@ routes.post('/sessions', createSession)
 routes.get('/users', getUsers)
 routes.post('/users', createUser)
 routes.delete('/users/:user_id', deleteUser)
+
+routes.post('/spots', createSpot)
 
 export default routes
