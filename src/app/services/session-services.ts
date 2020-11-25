d3c574cbf740acc2517bd019c88c41860cd3244f
Rafael Fischer <rafaelsanfischer@gmail.com>
2020-11-24 22:22:43 -0300

    refactored session to use user repository

--- src/app/services/session-services.ts
+++ src/app/services/session-services.ts
@@ -1,8 +1,8 @@
-import User from '../../database/models/User'
 import { formatResponse } from '../helpers'
+import { getUserByEmailRepository } from '../repositories/user-repository'
 
 export async function createSessionService(email: string) {
-    const user = await User.findOne({ email })
+    const user = await getUserByEmailRepository(email)
     if (!user) return formatResponse(404, { message: 'Not found' })
     return formatResponse(200, user)
 }
